68381dcd5a1463aa0770d44ae53f8b4cddcdac41
Ormon-huehuehue <120156766+Ormon-huehuehue@users.noreply.github.com>
2022-12-22 10:09:38 +0530

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -3,4 +3,4 @@ SID=int(input("Enter your SID : "))
 department=str(input("Department : "))
 CGPA= 9.9
 
-print("Hey, %s Here! \nMy SID is %d\nI am from %s department and my CGPA is %f " % (name,SID,department,CGPA))
+print("\n\nHey, %s Here! \nMy SID is %d\nI am from %s department and my CGPA is %f " % (name,SID,department,CGPA))
